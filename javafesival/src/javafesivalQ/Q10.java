add8b08799b668204756502141540ea6fa13595e
smhrd <smhrd@192.168.219.227>
2025-10-23 11:36:12 +0900

    이서 커밋 !

--- javafesival/src/javafesivalQ/Q10.java
+++ javafesival/src/javafesivalQ/Q10.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Q10 {
-
 	public static void main(String[] args) {
-		
+		//이서커밋
 		int[]arr=new int[8];
 		Random rd = new Random();
 		
